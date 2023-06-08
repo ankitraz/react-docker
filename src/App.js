18c8d67651f6f5217b39be51bb0cf66a716829f6
Ankit <ankit.raz@outlook.com>
2023-06-08 07:29:15 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i was changed!
+          hello world
         </p>
         <a
           className="App-link"
